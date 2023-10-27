d4108e5de56efa5a3ef01145005715c235648ee6
mianfazal01 <fazalkareemfyp@gmail.com>
2023-10-27 16:18:48 +0500

    Whatsapp exception handler

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Linking } from "react-native";
+import { Alert, Linking } from "react-native";
 
 export const openWhatsApp = () => {
   const whatsappUrl = "whatsapp://send?phone=++96181668315";
@@ -8,8 +8,7 @@ export const openWhatsApp = () => {
     if (supported) {
       Linking.openURL(whatsappUrl);
     } else {
-      console.log("WhatsApp is not installed on this device.");
-      // You can provide fallback behavior here.
+      Alert.alert("WhatsApp is not installed on this device.");
     }
   });
 };
